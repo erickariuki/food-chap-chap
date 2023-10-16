9de90a5ece84af891040d48894bf4fc4f379f3a0
yung <sitieneifranklin@gmail.com>
2023-10-16 22:52:46 +0300

    Follow Section is UptoDate

--- server/router/postRoutes.js
+++ server/router/postRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createPost } from '../controllers/postController.js';
+import Auth from '../middleware/auth.js'
 
 const postRouter = express.Router();
 
-postRouter.post('/create', createPost)
+postRouter.post('/create', Auth, createPost)
 
 export default postRouter;
