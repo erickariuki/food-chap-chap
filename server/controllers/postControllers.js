8aa1a84a363e2620bf1529c3996d8abfc46036f7
yung <sitieneifranklin@gmail.com>
2023-10-16 22:52:46 +0300

    Follow Section is UptoDate

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,7 +1,34 @@
+import User from '../model/userModel.js'
+import Post from '../model/postModel.js'
+
 const createPost = async (req, res) => {
     try {
-        
+        const {postedBy, text, image} = req.body;
+
+        if(!postedBy || !text) {
+            return res.status(400).json ({ message: "PostedBy and text fields are required "});
+        }
+
+        const user = await User.findById(postedBy);
+        if(!user) {
+            return res.status(404).json ({ message: "User not found "});
+        }
+
+        if(user._id.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ error: "User not found"});
+        }
+
+        const maxLength = 500;
+        if (text.length > maxLength) {
+            return res.status(400).json({ error: `Text must be less than ${maxLength} characters`});
+        }
+
+        const newPost = new Post({text, image, postedBy});
+        await newPost.save();
+
+        res.status(201).json({ message: "Post created successfully", newPost });
     } catch (error) {
-        
+        res.status(500).json({ message: error.message });
+        console.log(error)
     }
 }
