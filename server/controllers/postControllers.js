10d368c6a721388a3ed132fe5706de8e1c65da1b
yung <sitieneifranklin@gmail.com>
2023-10-17 00:00:42 +0300

    Post Section is UptoDate

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -40,11 +40,58 @@ const getPost = async (req, res) => {
         if(!post){
             return res.status(404).json ({ message: "Post not found "});
         }
-        
+
         res.status(200).json({ post });
     } catch (error) {
         res.status(500).json({ message: error.message});
     }
 };
 
-export { createPost, getPost};
+
+const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params._id);
+        if (!post){
+            return res.status(404).json ({ message: "Post not found "});
+        }
+
+        if(post.postedBy.toString()!== req.user._id.toString()) {
+            return res.status(401).json({ error: "Unauthorized to delete post"});
+        }
+
+        await Post.findByIdAndDelete(req.params._id);
+         res.status(200).json({ message: "Post deleted successully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}; 
+
+const likeUnlikePost = async (req, res) => {
+    try {
+        const {id:postId} = req.params;
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId);
+
+        if(!post) {
+            return res.status(404).json ({ message: "Post not found "});
+        }
+
+        const userLikedPost = post.likes.includes(userId);
+        if(userLikedPost) {
+            //unlike post
+            await Post.updateOne({_id:postId}, {$pull: {likes: userId}});
+            res.status(200).json({ message: "Post unliked successfully" });
+        } else {
+            // like post
+            post.likes.push(userId);
+            await post.save();
+            res.status(200).json({ message: "Post liked successfully"});
+        }
+    } catch (error) {
+        
+    } 
+}
+
+
+export { createPost, getPost, deletePost, likeUnlikePost};
--- server/router/postRoutes.js
+++ server/router/postRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createPost, getPost } from '../controllers/postController.js';
+import { createPost, getPost, deletePost, likeUnlikePost } from '../controllers/postController.js';
 import Auth from '../middleware/auth.js'
 
 const postRouter = express.Router();
 
-postRouter.get('/:id', getPost)
-postRouter.post('/create', Auth, createPost)
+postRouter.get('/:id', getPost);
+postRouter.post('/create', Auth, createPost);
+postRouter.delete('/:id', Auth, deletePost);
+postRouter.post('/like/:id', Auth, likeUnlikePost);
 
 export default postRouter;
