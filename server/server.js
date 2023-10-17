42d685c351ee48209e2da9517ec9119c7bcb35c3
yung <sitieneifranklin@gmail.com>
2023-10-17 14:07:08 +0300

    Post Section is UptoDate

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import express from 'express';
 import connect from './database/conn.js';
 import router from './router/routes.js';
-import cookieParser from "cookie-parser";
 import userRouter from './router/userRoutes.js';
 import postRouter from './router/postRoutes.js';
 
@@ -12,7 +11,6 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(cors());
 app.use(morgan("tiny"));
 app.disable("x-powered-by");
