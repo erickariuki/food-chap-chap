1a0faa2f875962957f19170307311784ddcd413b
yung <sitieneifranklin@gmail.com>
2023-10-17 09:50:09 +0300

    Post Section is UptoDate

--- server/router/postRoutes.js
+++ server/router/postRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { createPost, getPost, deletePost, likeUnlikePost } from '../controllers/postController.js';
+import { createPost, getPost, deletePost, likeUnlikePost, getFeedPosts } from '../controllers/postController.js';
 import Auth from '../middleware/auth.js'
 
 const postRouter = express.Router();
 
+postRouter.get('/feed', Auth, getFeedPosts);
 postRouter.get('/:id', getPost);
 postRouter.post('/create', Auth, createPost);
 postRouter.delete('/:id', Auth, deletePost);
 postRouter.post('/like/:id', Auth, likeUnlikePost);
+postRouter.post('/reply/:id', Auth, replyToPost);
 
 export default postRouter;
