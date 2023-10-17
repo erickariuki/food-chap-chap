713502fd0d152e18f670017c9875ca8dbe057ef6
yung <sitieneifranklin@gmail.com>
2023-10-17 14:07:08 +0300

    Post Section is UptoDate

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(cors());
 app.use(morgan("tiny"));
 app.disable("x-powered-by");
