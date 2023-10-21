1085c1257eadc7ffa74aad33355bd9eac2a5bd80
yung <sitieneifranklin@gmail.com>
2023-10-21 14:31:12 +0300

    Post Section is UptoDate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import { Layout } from 'antd';
+import Logo from './components/Logo';
+import MenuList from './components/MenuList';
 
+const { Header, Sider } = Layout;
 function App() {
 
   return (
     <>
-      
+      <Layout>
+        <Sider className='sidebar'>
+          <Logo />
+          <MenuList />
+        </Sider>
+      </Layout>
     </>
   )
 }
