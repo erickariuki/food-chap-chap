d19720d8139a65ba4a8f1dd08f0b3b2ee3170313
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-10-23 21:52:57 +0300

    conn.js: debuging

--- server/router/blogRoutes.js
+++ server/router/blogRoutes.js
@@ -1,1 +1,22 @@
 // blog routes
+import express from 'express';
+import  { createBlog, getAllBlog, getBlog, updateBlog, deleteBlog } from "../controllers/blogController.js"
+
+const blogRouter = express.Router();
+
+// Creating a new Blog
+blogRouter.post("/blogs", createBlog);
+
+// Get all Blogs
+blogRouter.get("/blogs", getAllBlog);
+
+// Get a Blog by ID
+blogRouter.get("/blogs/:_id", getBlog);
+
+// Update a Blog by ID
+blogRouter.put("/blogs/:_id", updateBlog);
+
+// Delete a Blog by ID
+blogRouter.delete("/blogs/:_id", deleteBlog);
+
+export default blogRouter;
