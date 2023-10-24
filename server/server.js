b5c07ba3ff28ec04fbb569d240ab2ccc141994b4
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-10-24 11:42:46 +0300

    🐛 fixing bug in blog controller

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connect from './database/conn.js';
 import router from './router/routes.js';
 import userRouter from './router/userRoutes.js';
 import postRouter from './router/postRoutes.js';
+import blogRouter from './router/blogRoutes.js';
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
 app.use("/api", router);
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter),
+app.use("/api/blogs", blogRouter);
 
 
 // start server only when mongodb connected
