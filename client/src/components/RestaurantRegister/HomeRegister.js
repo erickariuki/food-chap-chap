e546add0ab411985299d35a2e69d32ee660d9f7b
tito-arch <therabbitplc@gmail.com>
2023-10-29 13:03:49 +0300

    CleanUp: improve code readerbility

--- client/src/components/RestaurantRegister/HomeRegister.js
+++ client/src/components/RestaurantRegister/HomeRegister.js
@@ -5,20 +5,21 @@ import "../css/HomeRegister.css";
 function HomeRegister() {
   return (
     <div className="home-register-wrapper">
-      <h1 >Join us at FoodChapChap to unclock your restaurant potential</h1>
+      <h1>Join us at FoodChapChap to unclock your restaurant potential</h1>
 
       <div className="home-register-bottom">
-      <p>
-        Are you tired of the everyday dining frustrations at your restaurant? We
-        understand the challenges you face, and we have a solution that can
-        change the game for you. FoodChapChap is your gateway to a new era of
-        restaurant service, focused on convenience, simplicity, and efficiency.
-      </p>
-      <button>
-        <Link to="/register+your=restaurant/create+restaurant=form">
-          register restaurant
-        </Link>
-      </button>
+        <p>
+          Are you tired of the everyday dining frustrations at your restaurant?
+          We understand the challenges you face, and we have a solution that can
+          change the game for you. FoodChapChap is your gateway to a new era of
+          restaurant service, focused on convenience, simplicity, and
+          efficiency.
+        </p>
+        <button className="button">
+          <Link to="/register+your=restaurant/create+restaurant=form">
+            Register Restaurant
+          </Link>
+        </button>
       </div>
     </div>
   );
