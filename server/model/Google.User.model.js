27ba74da6f0560fbb0d6c77cf624cbde969dbfbb
nutcas3 <mauricee423@gmail.com>
2023-10-29 20:51:27 +0300

    ✨ google user added

---
+++ server/model/Google.User.model.js
@@ -0,0 +1,31 @@
+import { Schema as _Schema, model } from "mongoose";
+let Schema = _Schema;
+
+const userSchema = new Schema({
+  id: {
+    type: String,
+    default: null,
+  },
+  email: {
+    type: String,
+    required: [true, "email required"],
+    unique: [true, "email already registered"],
+  },
+  firstName: String,
+  lastName: String,
+  profilePhoto: String,
+  password: String,
+  source: {
+    type: String,
+    required: [true, "source not specified"],
+  },
+  lastVisited: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+var userModel = model("user", userSchema, "user");
+
+export default userModel;
+
