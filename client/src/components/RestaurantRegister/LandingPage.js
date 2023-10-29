f135221f78818ccaa8ed9f35d518c82fc52fcee3
tito-arch <therabbitplc@gmail.com>
2023-10-29 13:07:12 +0300

    Clean Up: remove unnecessary imports

--- client/src/components/RestaurantRegister/LandingPage.js
+++ client/src/components/RestaurantRegister/LandingPage.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import HomeRegister from './HomeRegister'
-import GettingStarted from './Gettingstarted'
-import WhyChoose from './WhyChoose'
-import JoinFoodChapChap from './JoinFoodChapChap'
-import FAQs from './FAQs'
+import React from "react";
+import HomeRegister from "./HomeRegister";
+import GettingStarted from "./Gettingstarted";
+import WhyChoose from "./WhyChoose";
+import FAQs from "./FAQs";
 
 function LandingPage() {
   const landingPageStyling = {
-    margin: '5em 10px',
+    margin: "5em 10px",
   };
   return (
     <div style={landingPageStyling}>
-        <HomeRegister />
-        <WhyChoose />
-        <GettingStarted />
-        <JoinFoodChapChap />
-        <FAQs />
-        
-
+      <HomeRegister />
+      <WhyChoose />
+      <GettingStarted />
+      <FAQs />
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
