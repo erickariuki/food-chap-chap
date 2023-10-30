b0a83897a5113a3ea5cb7885cb701e5c787bedc7
yung <sitieneifranklin@gmail.com>
2023-10-30 11:30:58 +0300

    Post Section is UptoDate📝

--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -8,6 +8,6 @@ router.get("/:id", Auth, getUser);
 router.put("/follow/:id", Auth, followUser);
 router.put("/unfollow", Auth, unfollowUser);
 router.put("/updatepic", Auth, updateProfilePic);
-router.post("/search-users", Auth, searchUsers);
+router.post("/searchUsers", Auth, searchUsers);
 
 export default router;
