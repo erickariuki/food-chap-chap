6994646cdc33075d47a3d23e89da5d49e0ace87a
yung <sitieneifranklin@gmail.com>
2023-11-07 13:39:59 +0300

    BLogs Tab is UptoDate📝

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './sidebar.css';
 import {
@@ -16,6 +16,11 @@ import CreatePostModal from '../CreatePost/CreatePostModal';
 
 export default function Sidebar() {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  const handleCollapseToggle = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
 
   const hideSidebar = () => {
     setModalOpen(false);
@@ -23,63 +28,72 @@ export default function Sidebar() {
 
   const handleAddPostClick = () => {
     setModalOpen(true);
+    setSidebarOpen(false);
   };
 
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+      <div className="collapse-button" onClick={handleCollapseToggle}>
+        ☰
+      </div>
+      {isSidebarOpen && (
       <div className="sidebarWrapper">
         <ul className="sidebarList">
           <li className="sidebarListItem">
-            <Link to="/" onClick={hideSidebar}>
+          <div onClick={(e) => { e.stopPropagation(); handleAddPostClick(); }} className="addPostButton">
               <RssFeed className="sidebarIcon" />
-              <span className="sidebarListItemText">Feed </span>
-            </Link>
+              {!isSidebarOpen && <span className="sidebarListItemText">Feed</span>}
+              </div>
+            
           </li>
           <li className="sidebarListItem">
             <Link to="/Chats">
               <Chat className="sidebarIcon" />
-              <span className="sidebarListItemText">Chats</span>
+              {!isSidebarOpen && <span className="sidebarListItemText">Chats</span>}
             </Link>
           </li>
           <li className="sidebarListItem">
             <Link to="/Following">
               <Group className="sidebarIcon" />
-              <span className="sidebarListItemText">Following</span>
+              {!isSidebarOpen && <span className="sidebarListItemText">Following</span>}
             </Link>
           </li>
           <li className="sidebarListItem">
             <Link to="/Notifications">
               <VibrationIcon className="sidebarIcon" />
-              <span className="sidebarListItemText">Notifications</span>
+              {!isSidebarOpen && <span className="sidebarListItemText">Notifications</span>}
             </Link>
           </li>
           <li className="sidebarListItem">
             <div onClick={handleAddPostClick} className="addPostButton">
               <AddCircleOutlineIcon className="sidebarIcon" />
-              <span className="sidebarListItemText">Upload</span>
+              {!isSidebarOpen && <span className="sidebarListItemText">Upload</span>}
             </div>
           </li>
           <li className="sidebarListItem">
             <Link to="/Profile">
               <AccountCircleIcon className="sidebarIcon" />
-              <span className="sidebarListItemText">Profile</span>
+              {!isSidebarOpen && <span className="sidebarListItemText">Profile</span>}
             </Link>
           </li>
           <li className="sidebarListItem">
             <Link to="/Settings">
               <SettingsIcon className="sidebarIcon" />
-              <span className="sidebarListItemText">Settings</span>
+              {!isSidebarOpen && <span className="sidebarListItemText">Settings</span>}
             </Link>
           </li>
           <li className="sidebarListItem">
             <Link to="/Report">
               <FlagIcon className="sidebarIcon" />
-              <span className="sidebarListItemText">Report</span>
+              {!isSidebarOpen && <span className="sidebarListItemText">Report</span>}
             </Link>
           </li>
         </ul>
+      
       </div>
+      )}
       {isModalOpen && <CreatePostModal closeModal={hideSidebar} />}
+      
     </div>
   );
 }
